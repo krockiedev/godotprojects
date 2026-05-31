9068c144e5d9afa6ac8531a423433d30808d28ce
Krockie <krockiedev@gmail.com>
2026-05-31 16:29:06 +0800

    Simple Script

--- gungeon-like/scenes/bullet.gd
+++ gungeon-like/scenes/bullet.gd
@@ -4,11 +4,14 @@ extends Area2D
 @export var direction: Vector2 = Vector2.LEFT
 @onready var sprite: Sprite2D = $Sprite2D
 @export var angle: float
+
 func _physics_process(delta: float) -> void:
 	position += direction.normalized() * SPEED * delta
-
+	
+	pass
 func _ready() -> void:
 	sprite.rotation = angle
+	
 func screen_exitted() -> void:
 	var tween_out = get_tree().create_tween()
 	tween_out.tween_property(self, "modulate", Color(1,1,1,0),0.5)\
