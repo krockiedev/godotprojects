5d86b8b1e26ebda4aa1acb16bdc4fb510e414f70
Krockie <krockiedev@gmail.com>
2026-05-31 14:22:50 +0800

    Updated Gun Script
    The gun now shoots in the correct direction.

--- gungeon-like/gun.gd
+++ gungeon-like/gun.gd
@@ -13,10 +13,17 @@ func _unhandled_input(event: InputEvent) -> void:
 			if playernode.can_shoot:
 				var map_node = get_tree().get_first_node_in_group("bullets")
 				var bullet_instance = BULLET.instantiate()
-				var bullet_direction = global_position.direction_to(get_global_mouse_position())
-				bullet_instance.direction = bullet_direction
-				bullet_instance.angle = bullet_direction.angle()
 				
+				var mouse_pos = get_global_mouse_position()
+				var gun_pos = global_position
+				
+				var delta_x = mouse_pos.x - gun_pos.x
+				var delta_y = mouse_pos.y - gun_pos.y
+				
+				var angle = atan2(delta_y, delta_x)
+				
+				bullet_instance.direction = Vector2(cos(angle), sin(angle))
+				bullet_instance.angle = angle
 				bullet_instance.position = playernode.position
 				map_node.add_child(bullet_instance)
 				playernode.shoot()
